cda4d63051e64d1a8287fe8d4c96a94c833a39cf
Owanate Amachree <amachreeowanate@gmail.com>
2019-07-12 18:29:12 +0100

    testing app

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongodb_url : process.env.MONGODB_URL,
-    jwt_secret: process.env.JWT_SECRET,
+    secret: process.env.JWT_SECRET,
     port : process.env.PORT || 4000,
     environment : process.env.NODE_ENV,
 }
